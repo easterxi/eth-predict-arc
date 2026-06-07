3495533dc850a7feeedff7467792bfdc90a57a3a
kaduanganu <143816872+kaduanganu@users.noreply.github.com>
2026-06-07 08:28:58 +0000

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ const CHAIN_CONFIG = {
 
     "eth-sepolia": {
       chainId: "0xaa36a7",
-      rpcUrl: "https://sepolia.drpc.org",
+      rpcUrl: "https://ethereum-sepolia-rpc.publicnode.com",
       name: "Ethereum Sepolia",
       explorer: "https://sepolia.etherscan.io",
       usdcAddress: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
